ed1833daf99f48acf8002dd518d0e466daaba061
endysusanto13 <endy.susanto@gmail.com>
2021-05-26 01:48:47 +0700

    Merge username and password checker function

--- app.rb
+++ app.rb
@@ -3,32 +3,21 @@ require 'digest'
 @login_database = IO.readlines("./login.txt")     # Convert all words in .txt files to array
 @login_database.map! { |word| word.chomp }        # Remove newline from all words
 
-def check_username(username)
-  @login_database.each_with_index do |data, index|
-    return true if (data.include? username + " || ")
-  end
-  return false
-end
-
-def check_password(password)
+def check_login(username, password)
   hashed_password = Digest::SHA2.hexdigest(password)
-  
+
   @login_database.each_with_index do |data, index|
-    return true if (data.include? " || " + hashed_password)
+    return true if (data.include? username + " || " + hashed_password)
   end
   return false
 end
-
 
 puts "Enter your username (case sensitive):"
 username = gets.chomp
 
-user_exist = check_username username
-
-
 puts "Enter your password (case sensitive):"
 password = gets.chomp
 
+user_exist = check_login(username,password)
 
-password_exist = check_password password
-puts password_exist
+user_exist ? (puts "Login is successful.") : (puts "Invalid username and/or password.")
