95331094d443fd33e183a7009c655b63e9b00d5a
endysusanto13 <endy.susanto@gmail.com>
2021-05-26 00:08:01 +0700

    Add hash.rb to hash passwords
    Script to convert strings to hash

---
+++ hash.rb
@@ -0,0 +1,10 @@
+require 'digest'
+
+puts "Enter password to be hashed (case sensitive):"
+password = gets.chomp
+
+def hash_password(new_password)
+  @hashed_password = Digest::SHA2.hexdigest(new_password)
+end
+
+puts hash_password password
