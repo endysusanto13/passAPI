1df1967f5fa500d72829109fcdde451faa8bd1db
endysusanto13 <endy.susanto@gmail.com>
2021-05-26 02:48:05 +0700

    Add feature to check user's location

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'digest'
+load './location_check.rb'
 
 @login_database = IO.readlines("./login.txt")     # Convert all words in .txt files to array
 @login_database.map! { |word| word.chomp }        # Remove newline from all words
@@ -12,12 +13,19 @@ def check_login(username, password)
   return false
 end
 
+def check_location(username)
+  username[0..1].upcase == $country
+end
+
 puts "Enter your username (case sensitive):"
 username = gets.chomp
 
 puts "Enter your password (case sensitive):"
 password = gets.chomp
 
-user_exist = check_login(username,password)
 
-user_exist ? (puts "Login is successful.") : (puts "Invalid username and/or password.")
+if check_login(username,password)
+  check_location(username) ? (puts "Login is successful. Welcome #{username}. You are currently in #{$country_name}.") : (puts "Login is unsuccessful as user is not located at the right country.")
+else
+  puts "Invalid username and/or password."
+end
--- location_check.rb
+++ location_check.rb
@@ -4,4 +4,5 @@ access_token = '1d565ebcf07e24'
 handler = IPinfo::create(access_token)
 
 details = handler.details()
-$country = details.country   # Obtain 2 letters country code
+$country = details.country              # Obtain 2 letters country code
+$country_name = details.country_name    # Obtain full country name
